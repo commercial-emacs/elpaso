986af9a2bf2a9b6b4c3317b22df6f61c8e321680
dickmao <none>
2021-04-23 11:08:03 -0400

    mockhub

---
+++ test/mockhub.com/package.git/lisp/utest.el
@@ -0,0 +1,31 @@
+;;; utest.el --- Jump to arbitrary positions in visible text and select text quickly. -*- lexical-binding: t -*-
+
+;; Copyright (C) 2015-2020  Free Software Foundation, Inc.
+
+;; Author: Masked Avenger <dontbotherme@pobox.com>
+;; Version: 0.5.0
+;; Package-Requires: ((emacs "24.1") (cl-lib "0.5") (ptest "0.4.0"))
+;; Keywords: point, location
+
+;; This file is part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+
+;;; Code:
+(provide 'utest)
+
+;;; utest.el ends here
